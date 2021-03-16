cdf85efed2007e0bda474e630c64a31717349ee0
jamesahn0720 <76964759+jamesahn0720@users.noreply.github.com>
2021-03-15 22:21:02 -0700

    Update StringRemover.java

--- StringRemover.java
+++ StringRemover.java
@@ -6,17 +6,61 @@ class StringRemover
 {
    private String remove;
    private String sentence;
+	
+	   public StringRemover() {
 
-	public StringRemover( String sen, String rem )
+                 sentence = "";
+
+                    remove = "";
+
+              }
+
+	public StringRemover( String sentence, String remove)
 	{
+	   this.sentence = sentence;
+
+           this.remove = remove;
 	}
+	
+	 public void setRemover(String s, String rem) {
+
+                    sentence = s;
+
+                     remove = rem;
+
+              }
 
 	public void removeStrings()
 	{
+		String cleaned = sentence;
+
+                int index = cleaned.indexOf(remove);
+
+                  while (index != -1 && cleaned.length() > 0) {
+
+                  String strBefore = "";
+
+                  if ((index - 1) >= 0) {
+
+                 strBefore = cleaned.substring(0, index - 1);
+
+               } else {
+
+                 strBefore = cleaned.substring(0, index);
+		  }
+			   String strAfter = cleaned.substring(index + remove.length());
+
+                         cleaned = strBefore + strAfter;
+
+                        index = cleaned.indexOf(remove);
+
+                           }
+
+                           return cleaned;
 	}
 
 	public String toString()
 	{
-		return "";
+		return sentence + " - String to remove" + remove;
 	}
 }
