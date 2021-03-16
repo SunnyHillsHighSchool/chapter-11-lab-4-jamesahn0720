685bee3cc743dfd38ed055fc0ea5029296dfaba7
jamesahn0720 <76964759+jamesahn0720@users.noreply.github.com>
2021-03-15 22:21:52 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,21 @@
 class Main {
   public static void main(String[] args) {
-      //add test cases 
+       StringRemover stringRemover = new StringRemover("xR-MxR-MHelloxR-M","R-M");
+
+                           System.out.println(stringRemover);
+
+                           System.out.println(stringRemover.removeStrings());
+
+                            stringRemover.setRemover("sxsssxssxsxssexssxsesss", "xs");
+
+                           System.out.println("\n\n"+stringRemover);
+
+                           System.out.println(stringRemover.removeStrings());
+
+                            stringRemover.setRemover("fuxqwexqwertyxqwexqwertyrtyxqwertyrtyn","qwerty");
+
+                           System.out.println("\n\n"+stringRemover);
+
+                           System.out.println(stringRemover.removeStrings());
   }
 }
