4694e6c57ab1d44f9f1c98ec088773078401db54
jamesahn0720 <76964759+jamesahn0720@users.noreply.github.com>
2021-05-21 12:23:07 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,21 +1,12 @@
 class Main {
   public static void main(String[] args) {
-       StringRemover stringRemover = new StringRemover("xR-MxR-MHelloxR-M","R-M");
-
-                           System.out.println(stringRemover);
-
-                           System.out.println(stringRemover.removeStrings());
-
-                            stringRemover.setRemover("sxsssxssxsxssexssxsesss", "xs");
-
-                           System.out.println("\n\n"+stringRemover);
-
-                           System.out.println(stringRemover.removeStrings());
-
-                            stringRemover.setRemover("fuxqwexqwertyxqwexqwertyrtyxqwertyrtyn","qwerty");
-
-                           System.out.println("\n\n"+stringRemover);
-
-                           System.out.println(stringRemover.removeStrings());
+   StringRemover test1 = new StringRemover("xR-MxR-MHelloxR-M", "R-M");
+      System.out.println(test1);
+      StringRemover test2 = new StringRemover("sxsssxssxsxssexssxsesss", "xs");
+      System.out.println(test2);
+       StringRemover test3 = new StringRemover("fuxqwexqwertyxqwexqwertyrtyxqwertyrtyn", "qwerty");
+       System.out.println(test3);
+      StringRemover test4 = new StringRemover("dogdogcatddodogdogdoggog", "dog");
+      System.out.println(test4);
   }
 }
